bd4a1bdb95322be0f6cce8e4c4229b2dffbce9e6
Md Sabit Islam Bhuiya <sib.sustswe@gmail.com>
2024-05-30 00:00:12 +0600

    Default handler fixed for CommonJs. Added logs to view endpoints. Issue fixed for windows.

--- src/index.ts
+++ src/index.ts
@@ -1,89 +1,86 @@
 import { readdirSync, statSync } from "fs";
-import { join } from "path";
+import path from "path";
+const currentWorkingDirectory = process.cwd()
 
-const HTTP_METHODS = ["GET", "PUT", "POST", "DELETE", "PATCH", "HEAD", "OPTIONS"];
+const HTTP_METHODS = [
+  "GET",
+  "PUT",
+  "POST",
+  "DELETE",
+  "PATCH",
+];
 
-async function getIndexFile(route: string) {
-  return await import(process.cwd() + "/" + route + "/index.js");
-}
+function getControllerFilePaths(dirPath: string): string[] {
+  const filePaths = readdirSync(dirPath);
+  let controllerFilePaths: string[] = []
+  filePaths.forEach(async (filePath) => {
+    const controllerPath = path.join(dirPath, filePath);
+    const isFolder = statSync(controllerPath).isDirectory();
+    if (isFolder) {
+      controllerFilePaths = controllerFilePaths.concat(getControllerFilePaths(controllerPath))
+      return
+    }
 
-function handleRouting(
-  route: string,
-  method: string,
-  handler: Function | Function[],
-  router: any
-) {
-  if (handler instanceof Function) {
-    router[method.toLowerCase()](route, handler);
-  } else if (handler instanceof Array<Function>) {
-    router[method.toLowerCase()](route, ...handler);
-  }
-}
-
-function getRouteType (file:string):RouteType {
-      let routeType:RouteType = "static";
-      if(file==="*"){routeType="catch-all"}
-      else if(file.startsWith(":")){routeType="dynamic"}
-      return routeType
-}
-
-type RouteType = 'dynamic' | 'static' | 'catch-all'
-
-function readIndexFiles(
-  dirPath: string,
-  indexPaths: string[] = [],
-) {
-  const files = readdirSync(dirPath);
-  files.forEach(function(file) {
-    const filePath = join(dirPath, file);
-    const stat = statSync(filePath);
-    if (stat.isDirectory()) {
-      readIndexFiles(filePath, indexPaths);
+    if (
+      (controllerPath.charAt(0) === "[" && controllerPath.charAt(controllerPath.length - 4) === "]") ||
+      controllerPath.charAt(0) === ":"
+    ) {
+      controllerFilePaths.push(controllerPath)
     } else {
-      if (file === "index.js") {
-          indexPaths.unshift(dirPath);
-      }
+      controllerFilePaths.unshift(controllerPath)
     }
   });
+  return controllerFilePaths;
+}
+
+function getApiRoute(url: string): string {
+  const [baseurl] = path.basename(url).split(".");
+  const dirname = path.dirname(url);
+  const apiRoute = baseurl === "index" ? dirname : path.join(dirname, baseurl);
+  return apiRoute
+    .split(path.sep)
+    .map((item) => {
+      if (item.startsWith("[") && item.endsWith("]")) {
+        return `:${item.substring(1, item.length - 1)}`;
+      }
+      return item;
+    })
+    .join(path.posix.sep);
 }
 
 type FolderRouterOptions = {
-  routeDir: string;
-  extraMethods: Array<string>;
+  routeDir?: string;
+  extraMethods?: Array<String>;
 };
 
+const isValidHandlers = (handlers: any[]) => {
+  return handlers.every(handler => typeof handler === 'function')
+}
+
 export async function configureFolderRouter(
-  router: any,
-  options: FolderRouterOptions
+  router: unknown,
+  options: FolderRouterOptions = {}
 ) {
-  if (router)
-    try {
-      if (options?.extraMethods && options.extraMethods instanceof Array<string>) {
-        options.extraMethods.forEach((method) => {
-          HTTP_METHODS.push(method.toUpperCase());
-        });
+  const { extraMethods = [], routeDir = "routes" } = options;
+  if (!router) {
+    return;
+  }
+  const httpMethods = [...HTTP_METHODS, ...extraMethods.map(extraMethod => extraMethod.toUpperCase())]
+  const controllerPaths = getControllerFilePaths(routeDir!);
+  for (let controllerPath of controllerPaths) {
+    const importPath = path.sep === path.posix.sep
+      ? path.join(currentWorkingDirectory, controllerPath)
+      : "file://" + path.join(currentWorkingDirectory, controllerPath);
+    const handlers = await import(importPath);
+    httpMethods.forEach((method) => {
+      const handler = handlers[method] || handlers.default?.default || handlers.default;
+      const apiRoute = getApiRoute(controllerPath.substring(routeDir.length));
+      const apiRouteHandlers = [handler].flat();
+      if (!isValidHandlers(apiRouteHandlers)) {
+        return
       }
-      const routeDir = options?.routeDir ?? "routes";
-      const indexPaths: string[] = [];
-      readIndexFiles(routeDir, indexPaths);
-      for (let indexFile of indexPaths) {
-        const handlers = await getIndexFile(indexFile);
-        HTTP_METHODS.forEach((method) => {
-          let handler = handlers
-            ? handlers[method] || handlers.default
-            : undefined;
-          if (handler) {
-            const apiRoute = indexFile.substring(routeDir.length);
-            handleRouting(
-              apiRoute,
-              method,
-              handler,
-              router
-            );
-          }
-        });
-      }
-    } catch (err) {
-      console.log("folder routing exception: ", err);
-    }
+      router[method.toLowerCase()](apiRoute, ...apiRouteHandlers);
+      console.log(`> ${method} ${apiRoute}`)
+    });
+  }
 }
